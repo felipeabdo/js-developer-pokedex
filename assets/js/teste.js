a2820216a945862719a1e3d83784921b59b25fbd
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2024-01-22 10:49:41 -0300

    Retirada do arquivo teste.js

--- assets/js/teste.js
+++
@@ -1,9 +0,0 @@
-const offset = 0;
-let limit = 10;
-
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemonList) => console.log(pokemonList))
