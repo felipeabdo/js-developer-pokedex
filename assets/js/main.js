eaaecd4d9859aaf102ead3714bfdab7356f08f2e
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2024-01-22 12:31:22 -0300

    Ajuste na cor do tipo de pokemon no card

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,8 +53,19 @@ function capitalizeFirstLetter(word) { // Função para transformar a primeira l
   return word.charAt(0).toUpperCase() + word.slice(1); // charAt(0): escolhe a a letra da posição colocada no argumento. Pense na palavra como um array de letras. toUpperCase(): transforma toda a palavra em maiúscula. + word.slice(1): o + concatena a letra transformada em maiúscula anteriormente com o resto da palavra obtido com o metodo slice(1) que retira a letra da posição escolhida na palavra.
 }
 
-function loadPokemonItens(offset, limit) { 
+function loadPokemonItens(offset, limit) { // Função para retornar cards de número específico de pokemons, formatados para HTML.
+
+  // Vamos na função getPokemons e então temos um array vazio de pokemons como default no argumento do .then pro precaução, mas esperando uma lista de pokemons de getPokemons.
+
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+
+
+    // Criamos uma variável newHTML que formatará cada pokemon da seguinte forma:
+    // Itererá por pokemon através do método map;
+    // Um li formatado será criado a cada pokemon buscado e dinamicamente buscando informações desses pokemons;
+    // O metodo join('') vai juntar todos esses pokemons em um grande array;
+    // pokemonList.innerHTML += newHTML vai adicionar cada li ao ol que foi pego em pokemonList.
+
     const newHTML = pokemons.map((pokemon) => `
     <li class="pokemon ${pokemon.type}">
 
@@ -84,9 +95,9 @@ function loadPokemonItens(offset, limit) {
   });
 }
 
-loadPokemonItens(offset, limit);
+loadPokemonItens(offset, limit); // Função pra carregar os primeiros  pokemons na página.
 
-loadMoreButton.addEventListener('click', () => {
+loadMoreButton.addEventListener('click', () => { // Espera uma ação pra fazer algo (click);
   offset += limit;
   loadPokemonItens(offset, limit);
 })
